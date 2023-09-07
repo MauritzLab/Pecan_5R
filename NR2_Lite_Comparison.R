623b16971f922835e6b4c1ae2012d983cd0bc40b
Martinez <vmartinez62@utep.edu>
2023-09-07 15:24:10 -0600

    update code to git

--- NR2_Lite_Comparison.R
+++ NR2_Lite_Comparison.R
@@ -6,7 +6,7 @@ library(ggplot2)
 library(lubridate)
 
 #Set Working directory
-setwd("C:/Users/vmartinez62/OneDrive - University of Texas at El Paso")
+setwd("C:/Users/vmartinez62/OneDrive - University of Texas at El Paso/Pecan_Tornillo_5R/ECTower/Maintenance/NR2_Lite_CalTest/")
 
 #Import file from datalogger
 data.head <- read.table("CR3000_Table1.dat", sep = ",", skip = 1, header = TRUE)[1,]
@@ -15,3 +15,9 @@ data <- read.table("CR3000_Table1.dat", sep = ",", skip = 4, header = FALSE, col
 #Format timestamp
 data <- data%>%
   mutate(datetime = ymd_hms(TIMESTAMP))
+
+# change data to long format
+data.long <- data %>%
+  pivot_longer(!c(TIMESTAMP,RECORD), names_to="variable",values_to="value") %>%
+  mutate(TIMESTAMP = ymd_hms(TIMESTAMP),
+         date=as.Date(TIMESTAMP))
