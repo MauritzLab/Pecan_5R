1365527e38e0c4957b4df22f6c895082808e6fb9
Martinez <vmartinez62@utep.edu>
2023-09-07 16:12:04 -0600

    added long format

--- NR2_Lite_Comparison.R
+++ NR2_Lite_Comparison.R
@@ -4,6 +4,7 @@
 library(dplyr)
 library(ggplot2)
 library(lubridate)
+library(tidyr)
 
 #Set Working directory
 setwd("C:/Users/vmartinez62/OneDrive - University of Texas at El Paso/Pecan_Tornillo_5R/ECTower/Maintenance/NR2_Lite_CalTest/")
@@ -16,20 +17,20 @@ data <- read.table("CR3000_Table1.dat", sep = ",", skip = 4, header = FALSE, col
 data <- data%>%
   mutate(datetime = ymd_hms(TIMESTAMP))
 
+#transform data to long format
+data.long <- data%>%
+  filter(datetime>ymd("2023-08-11"))%>%
+  select(!c(TIMESTAMP, RECORD))%>%
+  pivot_longer(!datetime, names_to = "sensor", values_to = "values")
+
 #graph time series from wide format
-ggplot(data, aes(x=datetime)) +
-geom_point(aes(y=NR_Wm2_Avg), color = "blue") +
-geom_line(aes(y=NR_Wm2_Avg), color = "blue") +
-geom_point(aes(y=NR_Wm2_2_Avg), color = "red") +
-geom_line(aes(y=NR_Wm2_2_Avg), color = "red") 
+ggplot(data.long, aes(x=datetime, y=values, color=sensor)) +
+geom_point() +
+geom_line() 
 
 #graph sensors as scatter plot
 ggplot(data, aes(x=NR_Wm2_Avg, y=NR_Wm2_2_Avg)) +
   geom_point() +
   geom_abline(slope=1, intercept=0)
 
-#transform data to long format
-data.long <- data%>%
-  filter(datetime>ymd("2023-08-11"))%>%
-  select(!c(TIMESTAMP, RECORD))%>%
-  pivot_longer(!datetime, names_to = "sensor", values_to = "values")
+
